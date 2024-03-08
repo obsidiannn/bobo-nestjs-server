cd598254ced3d4335a5f59be74afdbdafd1c0403
sunjx <raymooonn@gmail.com>
2024-03-08 14:09:23 +0800

    fix

--- src/modules/common/services/firebase.service.spec.ts
+++ src/modules/common/services/firebase.service.spec.ts
@@ -4,6 +4,7 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { SystemService } from '@/modules/common/services/system.service'
 import { FirebaseService } from './firebase.service'
 import { Message } from 'firebase-admin/messaging'
+import { randomInt } from 'crypto'
 describe('firebase test', () => {
   let app: NestExpressApplication
   let firebaseService: FirebaseService
@@ -23,12 +24,16 @@ describe('firebase test', () => {
   })
 
   describe('firebase 消息', () => {
-    const token = 'dVZQjSwQQceR0uD9wOVQvH:APA91bE_MXlW-55fo4CHsTaGht0Slhf0kMGyMgvvEJQclRDRGX7bI212J4V4uOIjwtBcoBgTNwwquxLfiUQA6a_vCnNbxrBdQKuBtmK3R0qKZbUqkuhycNBz49_cFdEx1HqSrKICEn9l'
+    // const token = 'dVZQjSwQQceR0uD9wOVQvH:APA91bE_MXlW-55fo4CHsTaGht0Slhf0kMGyMgvvEJQclRDRGX7bI212J4V4uOIjwtBcoBgTNwwquxLfiUQA6a_vCnNbxrBdQKuBtmK3R0qKZbUqkuhycNBz49_cFdEx1HqSrKICEn9l'
+    const token = 'dGl0wDX6R7eIcgef33PPeC:APA91bEOeChUYmMsIpYOwI1ZBtzJce8fS2HPyYv4n4rV8jwW1sRwe68rnCTi2Sn9C9Mv3m9Ccj_kkoW-r5cSJPuC-oS5IpOgOKqZEtR4_5PsG3xTR6c9aHwV3ELXaluOYHO-bQ7TGGMP'
+    
     const message: Message = {
       notification: {
-        title: 'Sparky says hello3'
+        title: '聊天消息' + randomInt(2).toString(),
+        body: '您收到了一条聊天消息'
       },
       android: {
+        // priority: 'high',
         notification: {
           imageUrl: 'https://foo.bar.pizza-monster.png'
         }
